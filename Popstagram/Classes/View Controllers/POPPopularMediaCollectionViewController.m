c2c481efb54b2f4a3eca5b471992f23d3d48b198
Mitchell Porter <mitchellporter@gmail.com>
2014-08-24 16:39:00 -0700

    Added collection view with corresponding nib

---
+++ Popstagram/Classes/View Controllers/POPPopularMediaCollectionViewController.m
@@ -0,0 +1,39 @@
+//
+//  POPPopularMediaCollectionViewController.m
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/24/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "POPPopularMediaCollectionViewController.h"
+
+@interface POPPopularMediaCollectionViewController ()
+
+@end
+
+@implementation POPPopularMediaCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
