8a163a15cd4852d880736daacb1881edbbd2912d
Mitchell Porter <mitchellporter@gmail.com>
2014-08-24 14:48:03 -0700

    Added instagram networking class

---
+++ Popstagram/Classes/Models/POPInstagramNetworkingClient.h
@@ -0,0 +1,13 @@
+//
+//  POPInstagramNetworkingClient.h
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/24/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface POPInstagramNetworkingClient : AFHTTPSessionManager
+
+@end
---
+++ Popstagram/Classes/Models/POPInstagramNetworkingClient.m
@@ -0,0 +1,13 @@
+//
+//  POPInstagramNetworkingClient.m
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/24/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "POPInstagramNetworkingClient.h"
+
+@implementation POPInstagramNetworkingClient
+
+@end
