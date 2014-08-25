143be9ecd58cdf17ca737bfd51487914e7726685
Mitchell Porter <mitchellporter@gmail.com>
2014-08-25 11:33:07 -0700

    Added Views group to project structure and created collection view cell subclass.

---
+++ Popstagram/Classes/Views/POPMediaCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  POPMediaCollectionViewCell.h
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/25/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POPMediaCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Popstagram/Classes/Views/POPMediaCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  POPMediaCollectionViewCell.m
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/25/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "POPMediaCollectionViewCell.h"
+
+@implementation POPMediaCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
