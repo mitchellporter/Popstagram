93784518b2e7f0224e1cdb28108780e466e4f608
Mitchell Porter <mitchellporter@gmail.com>
2014-08-25 21:41:29 -0700

    Added POPMediaDisplayViewController class with corresponding storyboard item

---
+++ Popstagram/Classes/View Controllers/POPMediaDisplayViewController.h
@@ -0,0 +1,15 @@
+//
+//  POPMediaDisplayViewController.h
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/25/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POPMediaDisplayViewController : UIViewController
+
+
+
+@end
