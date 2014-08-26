ba311fda50e26255a90167eb6bea837cdbb599b3
Mitchell Porter <mitchellporter@gmail.com>
2014-08-25 18:20:59 -0700

    Added second collection vc for tagged media

---
+++ Popstagram/Classes/View Controllers/POPTaggedMediaCollectionViewController.m
@@ -0,0 +1,41 @@
+//
+//  POPTaggedMediaCollectionViewController.m
+//  Popstagram
+//
+//  Created by Mitchell Porter on 8/25/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "POPTaggedMediaCollectionViewController.h"
+
+@interface POPTaggedMediaCollectionViewController ()
+
+@end
+
+@implementation POPTaggedMediaCollectionViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
