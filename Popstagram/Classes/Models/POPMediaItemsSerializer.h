a715b4c8913351b1103e2a525a930cf95e2bdabe
Mitchell Porter <mitchellporter@gmail.com>
2014-09-03 15:52:11 -0700

    Added POPMediaItemsSerializer

---
+++ Popstagram/Classes/Models/POPMediaItemsSerializer.h
@@ -0,0 +1,13 @@
+//
+//  POPMediaItemsSerializer.h
+//  Popstagram
+//
+//  Created by Mitchell Porter on 9/3/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "AFURLResponseSerialization.h"
+
+@interface POPMediaItemsSerializer : AFJSONResponseSerializer
+
+@end
---
+++ Popstagram/Classes/Models/POPMediaItemsSerializer.m
@@ -0,0 +1,13 @@
+//
+//  POPMediaItemsSerializer.m
+//  Popstagram
+//
+//  Created by Mitchell Porter on 9/3/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "POPMediaItemsSerializer.h"
+
+@implementation POPMediaItemsSerializer
+
+@end
